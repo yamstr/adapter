0fef87cdc06bdafd8daf10ae1af44a3896c62578
Andrey Volkovitskiy <yamstr@MacBook-Air-Andrey.local>
2017-11-12 01:57:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const crypto = require('crypto');
 const config = require('config');
+const request = require('request');
 const koa = require('koa');
 const koaBody = require('koa-body')();
 const koaLogger = require('koa-logger')();
@@ -9,8 +11,52 @@ const koaStatic = require('koa-static');
 const koaViews = require('koa-views');
 const app = new koa();
 
-koaRouter.get('/', async (ctx, next) => {
+class Adapter {
+	static getToken(chat_id) {
+		return crypto.createHash('md5').update((process.env.SALT || config.salt) + chat_id).digest('hex');
+	}
+
+	static getWebHookURL(chat_id) {
+		return `https://adapter.yamstr.com/${this.getToken(chat_id)}/${chat_id}`;
+	}
+
+	static sendMessage(chat_id, text) {
+		request.post({
+			url: `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN || config.telegram.token}/sendMessage`,
+			form: {
+				chat_id: chat_id,
+				text: text
+			}
+		}, (error, response, body) => {
+			if (error) {
+				console.error(error);
+			} else {
+				console.log(body);
+			}
+		});
+	}
+}
+
+koaRouter.post('/webhook', async (ctx, next) => {
+	if (['/start', '/getwh', '/getwh@adapterbot'].includes(ctx.request.body.message.text)) {
+		Adapter.sendMessage(ctx.request.body.message.chat.id, `WebHook URL: ${Adapter.getWebHookURL(ctx.request.body.message.chat.id)}?text=hello`);
+	}
+
 	ctx.status = 200;
+});
+
+koaRouter.all('/:token/:chat_id', koaBody, async (ctx, next) => {
+	let text;
+
+	if (ctx.method == 'GET') text = ctx.request.query.text;
+	if (ctx.method == 'POST') text = ctx.request.body.text;
+
+	if (ctx.params.token == Adapter.getToken(ctx.params.chat_id)) {
+		Adapter.sendMessage(ctx.params.chat_id, text);
+		ctx.status = 200;
+	} else {
+		ctx.status = 400;
+	}
 });
 
 app.use(async (ctx, next) => {
